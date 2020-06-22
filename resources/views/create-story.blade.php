26e61b168082b6d582db6124e1e65c4bb5cb7fb8
zerobity <zerobity.empresa@gmail.com>
2020-06-22 20:29:09 -0300

    create story
    Signed-off-by: zerobity <zerobity.empresa@gmail.com>

---
+++ resources/views/create-story.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('title', __('Nueva Historia'))
+
+@section('content')
+<div>
+    <example-component></example-component>
+    <story-creator></story-creator>
+</div>
+@endsection
