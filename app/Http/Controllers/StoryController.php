34ba1d6764316b5886793388ab39654160642c4e
SARQUIS-BOUTROS <sarquisboutroshector@gmail.com>
2020-07-01 23:19:20 +0000

    section's history information updated

--- app/Http/Controllers/StoryController.php
+++ app/Http/Controllers/StoryController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Story;
 use App\Part;
+use App\Category;
+use App\User;
 use Illuminate\Http\Request;
 
 class StoryController extends Controller
@@ -11,10 +13,19 @@ class StoryController extends Controller
     public function getStory($storyId)
     {
         $story = Story::where('id', $storyId)->first();
+        $creatorId = $story ->user_id ;
+        $creator = User::where('id', $creatorId)->first();
+        $categoryId = $story->category_id;
+        $category = Category::where('id',$categoryId)->get();
+        $categoryName = $story->category->name;
+       // dd($category)
         $parts = Part::where('story_id', $storyId)->get();
-        //dd($parts);
+        //dd($story->language);
         return view('sections')->with([
             'parts' => $parts, 
+            'story' => $story,
+            'category' => $categoryName,
+            'creator' => $creator
             ]);
     }
 }
